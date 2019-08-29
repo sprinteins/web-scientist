d05dd0d572863d40c9894a0fa02d568999ec1dd5
Luka Domovic <Luka.domovic1@gmail.com>
2019-08-29 16:03:36 +0200

    Change Fors

--- src/server/main_test.go
+++ src/server/main_test.go
@@ -58,11 +58,11 @@ func Test_By_Failed_Experiment_Reference_Sent(t *testing.T) {
 	var message = "TeSt"
 	var payload = []byte(message)
 
-	panic("We are here")
-
+	
 	var resp, err = http.Post(scientist.Address(), "text/plain", bytes.NewBuffer(payload))
 	Ok(t, err)
-
+	panic("We are here")
+	
 	var header = resp.Header.Get("X-WebScientist")
 	Equals(t, "WebScientist", header)
 
